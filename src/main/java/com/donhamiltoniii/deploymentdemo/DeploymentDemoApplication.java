cc887658d683786dfc9cd36f62e31981f7e61ee6
Columbus Instructor <instructor@wecancodeit.org>
2019-04-23 13:45:11 -0400

    Initial Commit

---
+++ src/main/java/com/donhamiltoniii/deploymentdemo/DeploymentDemoApplication.java
@@ -0,0 +1,13 @@
+package com.donhamiltoniii.deploymentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploymentDemoApplication.class, args);
+	}
+
+}
